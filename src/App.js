930c84d7dc345d1aaa584b150a72236670d36d0d
TheodorCristian <theodorc.petre@gmail.com>
2021-04-05 18:08:36 +0300

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -11,20 +11,53 @@ const clarifai = new Clarifai.App({
 });
 
 const particlesOptions = {
-  particles: {
-    line_linked: {
-      shadow: {
-        enable: true,
-        color: "#000000",
-        blur: 5
-      }
+  "particles": {
+    "number": {
+        "value": 160,
+        "density": {
+            "enable": false
+        }
     },
-    number: {
-      value: 60,
-      density: {
-        enable: true,
-        value_area: 600
-      }
+    "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+            "speed": 4,
+            "size_min": 0.3
+        }
+    },
+    "line_linked": {
+        "enable": false
+    },
+    "move": {
+        "random": true,
+        "speed": 1,
+        "direction": "top",
+        "out_mode": "out"
+    }
+},
+"interactivity": {
+    "events": {
+        "onhover": {
+            "enable": true,
+            "mode": "bubble"
+        },
+        "onclick": {
+            "enable": true,
+            "mode": "repulse"
+        }
+    },
+    "modes": {
+        "bubble": {
+            "distance": 250,
+            "duration": 2,
+            "size": 0,
+            "opacity": 0
+        },
+        "repulse": {
+            "distance": 400,
+            "duration": 4
+        }
     }
   }
 };
